c8712d061ddee35ed1a09ec6cf232141c804028b
Yasemin Atay <y.almila@gmail.com>
2021-01-17 00:03:58 +0300

    Add project files.

---
+++ ED_YGYK_Github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ED_YGYK_Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
